ddc02aeb75638a1620ba05d60190b42ac80cae50
WildCarlUK <69331291+WildCarl@users.noreply.github.com>
2022-07-15 16:15:27 +0000

    Added wcukReports

---
+++ README.md
@@ -0,0 +1,10 @@
+1. Edit config.lua to your liking.
+
+If you don't have the wcukNotify script you need to change the notifications on client.lua and on server.lua.
+
+And you are ready to go :)
+
+If you have any questions please contact us at Discord.com 
+https://discord.gg/GZnFrxRnRK
+
+Future updates and plans will also be available on discord
---
+++ server.lua
@@ -0,0 +1,243 @@
+local oneSync = false
+ESX = nil
+
+Citizen.CreateThread(function()
+	if GetConvar("onesync") ~= 'off' then
+		oneSync = true
+	end
+	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj; end)
+end)
+
+-------------------------- VARS
+
+local Webhook = ''
+local staffs = {}
+local FeedbackTable = {}
+
+-------------------------- NEW FEEDBACK
+
+RegisterNetEvent("wcukReports:NewFeedback")
+AddEventHandler("wcukReports:NewFeedback", function(data)
+	local identifierlist = ExtractIdentifiers(source)
+	local newFeedback = {
+		feedbackid = #FeedbackTable+1,
+		playerid = source,
+		identifier = identifierlist.license:gsub("license2:", ""),
+		subject = data.subject,
+		information = data.information,
+		category = data.category,
+		concluded = false,
+		discord = "<@"..identifierlist.discord:gsub("discord:", "")..">"
+	}
+
+	FeedbackTable[#FeedbackTable+1] = newFeedback
+
+	TriggerClientEvent("wcukReports:NewFeedback", -1, newFeedback)
+
+	if Webhook ~= '' then
+		newFeedbackWebhook(newFeedback)
+	end
+end)
+
+-------------------------- FETCH FEEDBACK
+
+RegisterNetEvent("wcukReports:FetchFeedbackTable")
+AddEventHandler("wcukReports:FetchFeedbackTable", function()
+	local staff = hasPermission(source)
+	if staff then
+		staffs[source] = true
+		TriggerClientEvent("wcukReports:FetchFeedbackTable", source, FeedbackTable, staff, oneSync)
+	end
+end)
+
+-------------------------- ASSIST FEEDBACK
+
+RegisterNetEvent("wcukReports:AssistFeedback")
+AddEventHandler("wcukReports:AssistFeedback", function(feedbackId, canAssist)
+	if staffs[source] then
+		if canAssist then
+			local id = FeedbackTable[feedbackId].playerid
+			if GetPlayerPing(id) > 0 then
+				local ped = GetPlayerPed(id)
+				local playerCoords = GetEntityCoords(ped)
+				local pedSource = GetPlayerPed(source)
+				local identifierlist = ExtractIdentifiers(source)
+				local assistFeedback = {
+					feedbackid = feedbackId,
+					discord = "<@"..identifierlist.discord:gsub("discord:", "")..">"
+				}
+
+				SetEntityCoords(pedSource, playerCoords.x, playerCoords.y, playerCoords.z)
+				TriggerClientEvent('wcukNotify:Alert', source, "REPORT", "You are assisting FEEDBACK #"..feedbackId.."!", 20000, 'info')
+				TriggerClientEvent('wcukNotify:Alert', id, "REPORT", "An admin arrived!", 20000, 'info')
+
+				if Webhook ~= '' then
+					assistFeedbackWebhook(assistFeedback)
+				end
+			else	
+				TriggerClientEvent('wcukNotify:Alert', id, "REPORT", "That player is no longer in the server!", 20000, 'error')
+			end
+			if not FeedbackTable[feedbackId].concluded then
+				FeedbackTable[feedbackId].concluded = "assisting"
+			end
+			TriggerClientEvent("wcukReports:FeedbackConclude", -1, feedbackId, FeedbackTable[feedbackId].concluded)
+		end
+	end
+end)
+
+-------------------------- CONCLUDE FEEDBACK
+
+RegisterNetEvent("wcukReports:FeedbackConclude")
+AddEventHandler("wcukReports:FeedbackConclude", function(feedbackId, canConclude)
+	if staffs[source] then
+		local feedback = FeedbackTable[feedbackId]
+		local identifierlist = ExtractIdentifiers(source)
+		local concludeFeedback = {
+			feedbackid = feedbackId,
+			discord = "<@"..identifierlist.discord:gsub("discord:", "")..">"
+		}
+
+		if feedback then
+			if feedback.concluded ~= true or canConclude then
+				if canConclude then
+					if FeedbackTable[feedbackId].concluded == true then
+						FeedbackTable[feedbackId].concluded = false
+					else
+						FeedbackTable[feedbackId].concluded = true
+					end
+				else
+					FeedbackTable[feedbackId].concluded = true
+				end
+				TriggerClientEvent("wcukReports:FeedbackConclude", -1, feedbackId, FeedbackTable[feedbackId].concluded)
+
+				if Webhook ~= '' then
+					concludeFeedbackWebhook(concludeFeedback)
+				end
+			end
+		end
+	end
+end)
+
+-------------------------- HAS PERMISSION
+
+function hasPermission(id)
+	local staff = false
+
+	if Config.ESX then
+		local player = ESX.GetPlayerFromId(id)
+		local playerGroup = player.getGroup()
+
+		if playerGroup ~= nil and playerGroup == "superadmin" or playerGroup == "admin" or playerGroup == "mod" then 
+			staff = true
+		end
+	else
+		for i, a in ipairs(Config.AdminList) do
+	        for x, b in ipairs(GetPlayerIdentifiers(id)) do
+	            if string.lower(b) == string.lower(a) then
+	                staff = true
+	            end
+	        end
+	    end
+	end
+
+	return staff
+end
+
+-------------------------- IDENTIFIERS
+
+function ExtractIdentifiers(id)
+    local identifiers = {
+        steam = "",
+        ip = "",
+        discord = "",
+        license = "",
+        xbl = "",
+        live = ""
+    }
+
+    for i = 0, GetNumPlayerIdentifiers(id) - 1 do
+        local playerID = GetPlayerIdentifier(id, i)
+
+        if string.find(playerID, "steam") then
+            identifiers.steam = playerID
+        elseif string.find(playerID, "ip") then
+            identifiers.ip = playerID
+        elseif string.find(playerID, "discord") then
+            identifiers.discord = playerID
+        elseif string.find(playerID, "license") then
+            identifiers.license = playerID
+        elseif string.find(playerID, "xbl") then
+            identifiers.xbl = playerID
+        elseif string.find(playerID, "live") then
+            identifiers.live = playerID
+        end
+    end
+
+    return identifiers
+end
+
+-------------------------- NEW FEEDBACK WEBHOOK
+
+function newFeedbackWebhook(data)
+	if data.category == 'player_report' then
+		category = 'Player Report'
+	elseif data.category == 'question' then
+		category = 'Question'
+	else
+		category = 'Bug'
+	end
+
+	local information = {
+		{
+			["color"] = Config.NewFeedbackWebhookColor,
+			["author"] = {
+				["icon_url"] = Config.IconURL,
+				["name"] = Config.ServerName..' - Logs',
+			},
+			["title"] = 'NEW FEEDBACK #'..data.feedbackid,
+			["description"] = '**Category:** '..category..'\n**Subject:** '..data.subject..'\n**Information:** '..data.information..'\n\n**ID:** '..data.playerid..'\n**Identifier:** '..data.identifier..'\n**Discord:** '..data.discord,
+			["footer"] = {
+				["text"] = os.date(Config.DateFormat),
+			}
+		}
+	}
+	PerformHttpRequest(Webhook, function(err, text, headers) end, 'POST', json.encode({username = Config.BotName, embeds = information}), {['Content-Type'] = 'application/json'})
+end
+
+-------------------------- ASSIST FEEDBACK WEBHOOK
+
+function assistFeedbackWebhook(data)
+	local information = {
+		{
+			["color"] = Config.AssistFeedbackWebhookColor,
+			["author"] = {
+				["icon_url"] = Config.IconURL,
+				["name"] = Config.ServerName..' - Logs',
+			},
+			["description"] = '**FEEDBACK #'..data.feedbackid..'** is being assisted by '..data.discord,
+			["footer"] = {
+				["text"] = os.date(Config.DateFormat),
+			}
+		}
+	}
+	PerformHttpRequest(Webhook, function(err, text, headers) end, 'POST', json.encode({username = Config.BotName, embeds = information}), {['Content-Type'] = 'application/json'})
+end
+
+-------------------------- CONCLUDE FEEDBACK WEBHOOK
+
+function concludeFeedbackWebhook(data)
+	local information = {
+		{
+			["color"] = Config.ConcludeFeedbackWebhookColor,
+			["author"] = {
+				["icon_url"] = Config.IconURL,
+				["name"] = Config.ServerName..' - Logs',
+			},
+			["description"] = '**FEEDBACK #'..data.feedbackid..'** has been concluded by '..data.discord,
+			["footer"] = {
+				["text"] = os.date(Config.DateFormat),
+			}
+		}
+	}
+	PerformHttpRequest(Webhook, function(err, text, headers) end, 'POST', json.encode({username = Config.BotName, embeds = information}), {['Content-Type'] = 'application/json'})
+end
